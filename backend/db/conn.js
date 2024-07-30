c1ae308c1d77e1da9ded0a6f2713e8b8880915e0
vansh-jain2002 <122167639+vansh-jain2002@users.noreply.github.com>
2024-07-30 12:30:20 +0530

    change

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,13 +1,38 @@
-// const mongoose = require('mongoose')
-// mongoose.connect("mongodb://127.0.0.1:27017/contact" ,{
-//    // useUnifiedTopology:true,
-//    // useNewUrlParser:true
-// }).then(() => {
-//     console.log("connection successfull")
-// }).catch((e) => {
-//     console.log(e)
-// })
+// // const mongoose = require('mongoose')
+// // mongoose.connect("mongodb://127.0.0.1:27017/contact" ,{
+// //    // useUnifiedTopology:true,
+// //    // useNewUrlParser:true
+// // }).then(() => {
+// //     console.log("connection successfull")
+// // }).catch((e) => {
+// //     console.log(e)
+// // })
 
+
+// require('dotenv').config();
+// const mongoose = require('mongoose');
+
+// const dbURI = process.env.MONGODB_URI;
+
+// if (!dbURI) {
+//   console.error("Error: MONGODB_URI is not defined in the .env file.");
+//   process.exit(1);
+// }
+
+// const connectWithRetry = () => {
+//   console.log('MongoDB connection with retry');
+//   mongoose.connect(dbURI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   }).then(() => {
+//     console.log('MongoDB is connected');
+//   }).catch(err => {
+//     console.error('MongoDB connection unsuccessful, retry after 5 seconds. ', err);
+//     setTimeout(connectWithRetry, 5000);
+//   });
+// };
+
+// connectWithRetry();
 
 require('dotenv').config();
 const mongoose = require('mongoose');
@@ -21,10 +46,7 @@ if (!dbURI) {
 
 const connectWithRetry = () => {
   console.log('MongoDB connection with retry');
-  mongoose.connect(dbURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }).then(() => {
+  mongoose.connect(dbURI).then(() => {
     console.log('MongoDB is connected');
   }).catch(err => {
     console.error('MongoDB connection unsuccessful, retry after 5 seconds. ', err);
@@ -33,3 +55,4 @@ const connectWithRetry = () => {
 };
 
 connectWithRetry();
+
